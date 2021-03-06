615cdd17a62118f798fa79c4f197c738f897f33d
Dylan Cawsey <dylancawsey@gmail.com>
2021-03-06 17:17:32 +1100

    Output env to console

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,6 +34,8 @@ const Layout = ({ isHomePage, children }) => {
 
       <main>{children}</main>
 
+      <script>console.log('process.env', process.env)</script>
+
       <body>
         <script src="/__/firebase/8.2.10/firebase-app.js"></script>
         <script src="/__/firebase/8.2.10/firebase-analytics.js"></script>
